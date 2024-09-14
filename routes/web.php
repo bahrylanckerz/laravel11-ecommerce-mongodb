72748449d1eafc4660ad00eaae246894686d6d75
Bahri <bahri@sgo.co.id>
2024-09-14 20:03:03 +0700

    integrate admin template

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('/admin')->namespace('App\Http\Controllers\Admin')->group(function(){
+    Route::match(['get','post'], 'dashboard', [AdminController::class, 'dashboard']);
 });
